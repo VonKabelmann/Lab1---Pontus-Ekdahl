4f6784a67fee8d5f51c32b3d4815ff8bd690aeec
Pontus Ekdahl <pontus.ekdahl@gmail.com>
2022-09-05 19:19:04 +0200

    Lab1 V1.0

--- Lab1 - Pontus Ekdahl/Program.cs
+++ Lab1 - Pontus Ekdahl/Program.cs
@@ -1,14 +1,39 @@
 ﻿
 
 string exampleStr = "29535123p48723487597645723645";
-string userInput = exampleStr;
-string outputColoured = String.Empty;
 
+Console.WriteLine("Input anything: ");
+string userInput = Console.ReadLine();
+int subStrLength;
+bool isValid;
+Console.ForegroundColor = ConsoleColor.White;
 
 for (int i = 0; i < userInput.Length; i++)
 {
-	for (int j = i; j < userInput.Length; j++)
+	subStrLength = 1;
+	isValid = false;
+	for (int j = i + 1; j < userInput.Length; j++)
 	{
-
+		if (char.IsDigit(userInput[j]))
+		{
+			subStrLength++;
+			if (userInput[j] == userInput[i])
+			{
+				isValid = true;
+				break;
+			}
+		}
+		else
+		{
+			break;
+		}
+	}
+	if (isValid)
+	{
+		Console.Write(userInput.Substring(0, i));
+		Console.ForegroundColor = ConsoleColor.Green;
+		Console.Write(userInput.Substring(i, subStrLength));
+		Console.ForegroundColor = ConsoleColor.White;
+		Console.WriteLine(userInput.Substring(i + subStrLength, userInput.Length - i - subStrLength));
 	}
 }
