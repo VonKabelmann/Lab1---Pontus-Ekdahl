55b5a819055bd2ee6559b18f5a07bc49fe7fa1ea
Pontus Ekdahl <pontus.ekdahl@gmail.com>
2022-09-06 18:41:46 +0200

    Tog bort bool isValid och kör printkoden när ett matchande tecken hittas istället för att ändra isValid till true

--- Lab1 - Pontus Ekdahl/Program.cs
+++ Lab1 - Pontus Ekdahl/Program.cs
@@ -6,14 +6,12 @@ Console.WriteLine("Input anything: ");
 //string userInput = Console.ReadLine();
 string userInput = exampleStr;
 int subStrLength;
-bool isValid;
 long sum = 0;
 Console.ForegroundColor = ConsoleColor.White;
 
 for (int i = 0; i < userInput.Length; i++)
 {
 	subStrLength = 1;
-	isValid = false;
 	for (int j = i + 1; j < userInput.Length; j++)
 	{
 		if (char.IsDigit(userInput[j]))
@@ -21,7 +19,12 @@ for (int i = 0; i < userInput.Length; i++)
 			subStrLength++;
 			if (userInput[j] == userInput[i])
 			{
-				isValid = true;
+				sum += Int64.Parse(userInput.Substring(i, subStrLength));
+				Console.Write(userInput.Substring(0, i));
+				Console.ForegroundColor = ConsoleColor.Green;
+				Console.Write(userInput.Substring(i, subStrLength));
+				Console.ForegroundColor = ConsoleColor.White;
+				Console.WriteLine(userInput.Substring(i + subStrLength, userInput.Length - i - subStrLength));
 				break;
 			}
 		}
@@ -29,15 +32,6 @@ for (int i = 0; i < userInput.Length; i++)
 		{
 			break;
 		}
-	}
-	if (isValid)
-	{
-		sum += Int64.Parse(userInput.Substring(i, subStrLength));
-		Console.Write(userInput.Substring(0, i));
-		Console.ForegroundColor = ConsoleColor.Green;
-		Console.Write(userInput.Substring(i, subStrLength));
-		Console.ForegroundColor = ConsoleColor.White;
-		Console.WriteLine(userInput.Substring(i + subStrLength, userInput.Length - i - subStrLength));
 	}
 }
 Console.WriteLine("");
