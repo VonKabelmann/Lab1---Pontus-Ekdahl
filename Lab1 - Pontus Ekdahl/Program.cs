8c779cbcec75deeac1eecd3d1d36fde1fd8b0dc8
Pontus Ekdahl <pontus.ekdahl@gmail.com>
2022-09-14 11:39:39 +0200

    Lade till kommentarer

--- Lab1 - Pontus Ekdahl/Program.cs
+++ Lab1 - Pontus Ekdahl/Program.cs
@@ -1,28 +1,31 @@
 ﻿
 
-string exampleStr = "29535123p48723487597645723645";
-
+//string exampleStr = "29535123p48723487597645723645";
+//string userInput = exampleStr;
 Console.WriteLine("Input anything: ");
-//string userInput = Console.ReadLine();
-string userInput = exampleStr;
-int subStrLength;
+string userInput = Console.ReadLine();
+
+int subStrLength;	// variabel för att hålla koll på längden/antal karaktärer i substringen som ska markeras
 long sum = 0;
-Console.ForegroundColor = ConsoleColor.White;
+Console.ForegroundColor = ConsoleColor.White; // för tydlighet
 
 for (int i = 0; i < userInput.Length; i++)
 {
 	subStrLength = 1;
 	for (int j = i + 1; j < userInput.Length; j++)
 	{
-		if (char.IsDigit(userInput[j]))
+		if (char.IsDigit(userInput[j]))	// om tecknet är en siffra
 		{
 			subStrLength++;
-			if (userInput[j] == userInput[i])
+			if (userInput[j] == userInput[i])	// om likvärdigt tecken hittas
 			{
 				sum += Int64.Parse(userInput.Substring(i, subStrLength));
+
 				Console.Write(userInput.Substring(0, i));
+
 				Console.ForegroundColor = ConsoleColor.Green;
 				Console.Write(userInput.Substring(i, subStrLength));
+
 				Console.ForegroundColor = ConsoleColor.White;
 				Console.WriteLine(userInput.Substring(i + subStrLength, userInput.Length - i - subStrLength));
 				break;
